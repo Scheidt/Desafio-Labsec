7420f3f0a523f6a2b576f96c8a7d33501e730229
Scheidt <pedrohenriqueprazeres@hotmail.com>
2024-10-28 01:17:26 -0300

    started on 5

---
+++ solution/tarefa-5/etapa-1/analise/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            Document document = builder.parse("src/main/resources/LU.xml");
+            document.getDocumentElement().normalize();
+
+            NodeList serviceList = document.getElementsByTagName("TSPService");
+
+            for (int i = 0; i < serviceList.getLength(); i++) {
+                Node serviceNode = serviceList.item(i);
+
+                if (serviceNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element serviceElement = (Element) serviceNode;
+
+                    String serviceTypeIdentifier = serviceElement
+                            .getElementsByTagName("ServiceTypeIdentifier")
+                            .item(0)
+                            .getTextContent();
+
+                    String serviceName = serviceElement
+                            .getElementsByTagName("ServiceName")
+                            .item(0)
+                            .getTextContent();
+
+                    System.out.println(serviceTypeIdentifier + ": " + serviceName);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e);
+        }
+    }
+}
