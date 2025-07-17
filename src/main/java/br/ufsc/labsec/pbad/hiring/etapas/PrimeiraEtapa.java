f753440fcb1a06e6aafde13f6a0a6bcb73489b03
Scheidt <pedrohenriqueprazeres@hotmail.com>
2025-07-16 23:28:23 -0300

    primeira etapa feita

--- src/main/java/br/ufsc/labsec/pbad/hiring/etapas/PrimeiraEtapa.java
+++ src/main/java/br/ufsc/labsec/pbad/hiring/etapas/PrimeiraEtapa.java
@@ -1,5 +1,10 @@
 package br.ufsc.labsec.pbad.hiring.etapas;
 
+import br.ufsc.labsec.pbad.hiring.Constantes;
+import Java.nio.file.Path;
+import Java.nio.file.Paths;
+import Java.nio.file.Files;
+import Java.util.HexFormat
 
 /**
  * <b>Primeira etapa - obter o resumo criptográfico de um documento</b>
@@ -23,7 +28,41 @@ package br.ufsc.labsec.pbad.hiring.etapas;
 public class PrimeiraEtapa {
 
     public static void executarEtapa() {
-        // TODO implementar
+
+        try {
+            MessageDigest hasher = MessageDigest.getInstance(Constantes.algoritmoResumo);
+
+            Path caminhoTexto = Paths.get(Constantes.caminhoTextoPlano);
+            Path caminhoOutput = Paths.get(Constantes.caminhoResumoCriptografico);
+
+            byte[] bytesTextoPlano = Files.readAllBytes(caminhoTexto);
+
+            byte[] hashed = hasher.digest(bytesTextoPlano);
+
+            String hashHex = hashPraHex(hashed);
+
+            Files.createDirectories(caminhoOutput.getParent());
+            Files.write(caminhoOutput, hashHex.getBytes());
+
+            System.out.println("Sucesso na primeira etapa :)");
+
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static String hashPraHex (byte[] hash){
+        HexFormat hexFormat = HexFormat.of();
+        return hexFormat.formatHex(hash);
     }
 
 }
+
+
+/**
+    public static final String caminhoTextoPlano =
+            caminhoArtefatos + "textos/textoPlano.txt";
+    public static final String caminhoResumoCriptografico =
+            caminhoArtefatos + "resumos/resumoTextoPlano.hex";
+*/
