b299c56a612a28885327780faaa5ab3c33010b93
Scheidt <pedrohenriqueprazeres@hotmail.com>
2026-04-06 18:57:59 -0300

    Style: cleanup

--- nivel4.py
+++ nivel4.py
@@ -1,22 +1,21 @@
 from cryptography.hazmat.primitives.serialization import pkcs12
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 
-# 1. Carregar a chave do .p12
+# Carregar a chave privada do arquivo PKCS#12
 with open("artefatos/nvl3/supervisor.p12", "rb") as f:
     private_key, cert, chain = pkcs12.load_key_and_certificates(f.read(), b"665098625")
 
-# 2. Extrair os números RSA raw
+# Extrair os parâmetros da chave RSA
 priv_numbers = private_key.private_numbers()
 d = priv_numbers.d
 n = priv_numbers.public_numbers.n
 
-# 3. Mensagem em hex → inteiro
+# Ler a mensagem hexadecimal do arquivo
 with open("artefatos/nvl4/number.txt", "r") as f:
     hex_message = f.read().strip()
     m = int(hex_message, 16)
 
-# 4. Assinatura textbook RSA (sem padding)
+# Gerar a assinatura RSA usando a chave privada e a mensagem
 sig = pow(m, d, n)
 
-print(f"Assinatura (int): {sig}")
 print(f"Assinatura (hex): {hex(sig)}")
