18ac117f13c114d76345d440f37b6fc64c776190
Scheidt <pedrohenriqueprazeres@hotmail.com>
2026-04-06 18:56:39 -0300

    finished nvl 4

--- nivel4.py
+++ nivel4.py
@@ -1,18 +1,21 @@
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
 
-# 1. Carregar a chave privada do .pem
-with open("chave.pem", "rb") as f:
-    private_key = load_pem_private_key(f.read(), password=b"sua_senha_aqui")
+# 1. Carregar a chave do .p12
+with open("artefatos/nvl3/supervisor.p12", "rb") as f:
+    private_key, cert, chain = pkcs12.load_key_and_certificates(f.read(), b"665098625")
 
-# 2. Extrair os números privados (d e n)
+# 2. Extrair os números RSA raw
 priv_numbers = private_key.private_numbers()
 d = priv_numbers.d
 n = priv_numbers.public_numbers.n
 
 # 3. Mensagem em hex → inteiro
-m = 0x169e9b087
+with open("artefatos/nvl4/number.txt", "r") as f:
+    hex_message = f.read().strip()
+    m = int(hex_message, 16)
 
-# 4. Assinatura RSA textbook (sem padding)
+# 4. Assinatura textbook RSA (sem padding)
 sig = pow(m, d, n)
 
 print(f"Assinatura (int): {sig}")
